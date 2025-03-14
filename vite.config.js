854f4cdd42fe5b906f5748c0f63d463304a91cf2
KoreanJoey <dolha4166@gmail.com>
2025-03-14 20:30:43 +1100

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Evaheld-trial/",
-  build: {
-    outDir: "dist",
-  },
 });
