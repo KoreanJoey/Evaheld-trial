ace26a08ece5b935afa4ace29b4726208a6f134d
KoreanJoey <dolha4166@gmail.com>
2025-03-14 15:36:58 +1100

    updated Sidebar in NavigationLayout

--- src/components/NavigationLayout.jsx
+++ src/components/NavigationLayout.jsx
@@ -8,16 +8,28 @@ const NavigationLayout = () => {
     <div>
       <Topbar />
       <div className="mt-[56px] ml-[138px]">
-        <div className="">
+        <div>
           <div className="flex gap-x-1 my-[15px]">
             <p className="text-[#5F348C]">Home</p>
             <p className="text-[#666666]">&gt;</p>
             <p className="text-[#666666]">Legacy Preservation</p>
           </div>
-          <div className="flex flex-col gap-3">
-            <SidebarItem icon={assets.dashboard_icon} label="Dashboard" />
-            <SidebarItem icon={assets.document_icon} label="Legacy Preservation" />
-            <SidebarItem icon={assets.userdocument_icon} label="Advance Care Planning" />
+          <div className="flex flex-col h-[70vh] justify-between">
+            <div className="flex flex-col gap-3">
+              <SidebarItem icon={assets.dashboard_icon} label="Dashboard" />
+              <SidebarItem
+                icon={assets.document_icon}
+                label="Legacy Preservation"
+              />
+              <SidebarItem
+                icon={assets.userdocument_icon}
+                label="Advance Care Planning"
+              />
+            </div>
+            <div className="flex flex-col gap-3">
+              <SidebarItem icon={assets.library_icon} label="My Library" />
+              <SidebarItem icon={assets.person_icon} label="My account" />
+            </div>
           </div>
         </div>
         <div></div>
