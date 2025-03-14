f338f3ce6e73991a1f9a898739be5007f9927c06
KoreanJoey <dolha4166@gmail.com>
2025-03-14 18:50:26 +1100

    updated NavigationRowTabs

---
+++ src/components/NavigationRowTabs.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const NavigationRowTabs = ({tabs}) => {
+
+    const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div className="border-b border-gray-300">
+      {/* 탭 목록 */}
+      <div className="flex space-x-6 text-gray-500 text-sm font-medium">
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={`relative py-3 ${
+              activeTab === index ? "text-black font-bold" : "hover:text-gray-700"
+            }`}
+            onClick={() => setActiveTab(index)}
+          >
+            {tab}
+
+            {/* 활성 탭 밑줄 */}
+            {activeTab === index && (
+              <span className="absolute left-0 bottom-0 w-full h-[2px] bg-[#5F348C]"></span>
+            )}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default NavigationRowTabs
--- src/pages/StoryLegacyPage.jsx
+++ src/pages/StoryLegacyPage.jsx
@@ -1,56 +1,69 @@
 import React from "react";
-import ProgressOverviewCard from "../components/\bProgressOverviewCard";
+import ProgressOverviewCard from "../components/ProgressOverviewCard";
 import NavigationLayout from "../components/NavigationLayout";
 import { assets } from "../assets/assets";
+import NavigationRowTabs from "../components/NavigationRowTabs";
+
+const tabs = [
+  "Family History & Story",
+  "Memoir & Truths",
+  "Legacy Letter & Statements",
+  "Memorialising a Loved One",
+  "Milestone Messages",
+  "My Own",
+];
 
 const StoryLegacyPage = () => {
   return (
     <div>
       <NavigationLayout title="Story & Legacy">
-      <div className="grid grid-flow-row grid-cols-3 gap-4">
-      <ProgressOverviewCard
-        icon={assets.storyLegacy_icon}
-        title="Personal Reflections"
-        progress={6}
-        total={12}
-        buttonText="Continue"
-      />
-      <ProgressOverviewCard
-        icon={assets.storyLegacy_icon}
-        title="Personal Reflections"
-        progress={6}
-        total={12}
-        buttonText="Continue"
-      />
-      <ProgressOverviewCard
-        icon={assets.storyLegacy_icon}
-        title="Personal Reflections"
-        progress={6}
-        total={12}
-        buttonText="Continue"
-      />
-      <ProgressOverviewCard
-        icon={assets.storyLegacy_icon}
-        title="Personal Reflections"
-        progress={6}
-        total={12}
-        buttonText="Continue"
-      />
-      <ProgressOverviewCard
-        icon={assets.storyLegacy_icon}
-        title="Personal Reflections"
-        progress={6}
-        total={12}
-        buttonText="Continue"
-      />
-      <ProgressOverviewCard
-        icon={assets.storyLegacy_icon}
-        title="Personal Reflections"
-        progress={6}
-        total={12}
-        buttonText="Continue"
-      />
-    </div>
+      <div className="flex justify-center items-start p-6">
+        <NavigationRowTabs tabs={tabs} />
+      </div>
+        <div className="grid grid-flow-row grid-cols-3 gap-4">
+          <ProgressOverviewCard
+            icon={assets.storyLegacy_icon}
+            title="Personal Reflections"
+            progress={6}
+            total={12}
+            buttonText="Continue"
+          />
+          <ProgressOverviewCard
+            icon={assets.storyLegacy_icon}
+            title="Personal Reflections"
+            progress={6}
+            total={12}
+            buttonText="Continue"
+          />
+          <ProgressOverviewCard
+            icon={assets.storyLegacy_icon}
+            title="Personal Reflections"
+            progress={6}
+            total={12}
+            buttonText="Continue"
+          />
+          <ProgressOverviewCard
+            icon={assets.storyLegacy_icon}
+            title="Personal Reflections"
+            progress={6}
+            total={12}
+            buttonText="Continue"
+          />
+          <ProgressOverviewCard
+            icon={assets.storyLegacy_icon}
+            title="Personal Reflections"
+            progress={6}
+            total={12}
+            buttonText="Continue"
+          />
+          <ProgressOverviewCard
+            icon={assets.storyLegacy_icon}
+            title="Personal Reflections"
+            progress={6}
+            total={12}
+            buttonText="Continue"
+          />
+        </div>
       </NavigationLayout>
     </div>
   );
